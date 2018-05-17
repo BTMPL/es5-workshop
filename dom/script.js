9e0cd92021e9a40a5bfaa939e5e326e735414a30
Bartosz Szczecinski <btm@anfo.pl>
2018-05-17 09:07:54 +0200

    Added DOM section

---
+++ dom/script.js
@@ -0,0 +1,13 @@
+/**
+ * (Nie pełny) przykład komunikacji AJAX.
+ *
+ * var xml = new XMLHttpRequest();
+ * xml.open('GET', 'http://example.com');
+ * xml.onreadystatechange = function() {
+ *   if(xml.readyState == 4) {
+ *     console.log(xml)  
+ *   }
+ * }
+ * xml.send();
+ * 
+*/
